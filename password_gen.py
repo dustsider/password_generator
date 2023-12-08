7338037cbe8f2191f7d173dfe19272c134b75537
dustsider <140916146+dustsider@users.noreply.github.com>
2023-12-07 17:47:59 -0800

    Add password generator program
    Simple program in python to generate passwords given an interger input

---
+++ password_gen.py
@@ -0,0 +1,27 @@
+#Python password generator 
+# 1) Import secrets, string, random to program for assistance to randomly choose among variables
+
+import secrets
+import string
+import random
+
+#Define password selection lists (abc, numbers, special characters and concatenate to a single string
+
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+selection_list = letters + digits + special_chars
+
+#Select length of password (set to eight for now/change for preferences)
+
+password_len = str(input("How long do you want your password to be? (integers only) "))
+
+#Initialize password string as empty.  
+# Use for loop with range of password length defined as above to add characters to password 
+# single quotations in loop to ensure no whitespace between characters
+
+password = ''
+for i in range (int(password_len)):
+    password  += ''.join(secrets.choice(selection_list))
+
+print(password)
